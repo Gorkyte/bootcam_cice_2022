83093f9ed3b6b4fa78c520429571b16a8de869db
Gorka <g.ormazabal@grupoprilux.com>
2022-03-23 16:45:33 +0100

    [WIP] - Added new project Slide menu -

---
+++ 15032022/SlideMenuApp/SlideMenuApp/SlideMenuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideMenuAppApp.swift
+//  SlideMenuApp
+//
+//  Created by Gorka Ormazabal on 18/3/22.
+//
+
+import SwiftUI
+
+@main
+struct SlideMenuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
